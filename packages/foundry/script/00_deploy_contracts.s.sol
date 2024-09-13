21f8de0d5fb15a85b7a4ed85d183324b7462a804
escottalexander@gmail.com <escottalexander@gmail.com>
2024-09-13 16:47:16 -0400

    Initial commit of rebasing-token

---
+++ packages/foundry/script/00_deploy_contracts.s.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "../contracts/RebasingERC20.sol";
+import "./DeployHelpers.s.sol";
+
+contract DeployContracts is ScaffoldETHDeploy {
+  function run() external ScaffoldEthDeployerRunner {
+    RebasingERC20 rebasingERC20 = new RebasingERC20();
+        console.logString(
+            string.concat(
+                "RebasingERC20 deployed at: ",
+                vm.toString(address(rebasingERC20))
+            )
+        );
+  }
+}
