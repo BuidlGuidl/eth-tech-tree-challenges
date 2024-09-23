3598e323f1313ef74c9df78d9c713664bcefc2b3
escottalexander@gmail.com <escottalexander@gmail.com>
2024-09-23 14:04:14 -0400

    only instantiate contract once

--- packages/foundry/test/RebasingERC20.t.sol
+++ packages/foundry/test/RebasingERC20.t.sol
@@ -57,12 +57,13 @@ contract RebasingERC20Test is Test {
     function setUp() public {
         luffy = address(this);
         zoro = address(0x123);
-        token = new RebasingERC20();
         // Use a different contract than default if CONTRACT_PATH env var is set
         string memory contractPath = vm.envOr("CONTRACT_PATH", string("none"));
         if (keccak256(abi.encodePacked(contractPath)) != keccak256(abi.encodePacked("none"))) {
             bytes memory contractCode = vm.getCode(contractPath);
             vm.etch(address(token), contractCode);
+        } else {
+            token = new RebasingERC20();
         }
         token.transfer(zoro, 1000 * 10 ** token.decimals());
         luffyBalance1 = token.balanceOf(luffy);
